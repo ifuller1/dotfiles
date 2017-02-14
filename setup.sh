2b6a82be585e3337205a0358e4ab05c98a22eeb6
Ian Fuller <dashdotian@gmail.com>
2017-02-14 21:13:57 +0000

    resurrect tmux

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ln -s  $DIR/tmux.conf ~/.tmux.conf 
 ln -s  $DIR/tmux.md ~/.tmux.md
+ln -s  $DIR/tmux ~/.tmux
 ln -s  $DIR/vimrc ~/.vimrc
 ln -s  $DIR/vim ~/.vim
 ln -s  $DIR/teamocil ~/.teamocil
